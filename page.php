8a6746c575268ab08e553ba6dd1665136d5d167b
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-03 22:43:50 +0600

    New page added

---
+++ page.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page Title</title>
+</head>
+<body>
+<?php $is_admin = false; ?>
+<?php if ($is_admin) : ?>
+    <a href="#">Edit</a>
+<?php endif; ?>
+<a href="#">View</a>
+</body>
+</html>
