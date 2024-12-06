0825a4ef03acd1e36bdb6cb5022c391667d935c3
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-06 16:02:20 +0600

    Post is done

--- newPage.php
+++ newPage.php
@@ -6,12 +6,19 @@
     <title>Testing Post Request</title>
 </head>
 <body>
-<form method="POST" action="./newPage.php">
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     Name: <input type="text" name="fname"/>
     <input type="submit">
 </form>
 <?php
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_REQUEST['fname']);
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo "Enter name is: $name";
+    }
+}
 ?>
 </body>
 </html>
