34a3c4d352040526c1e985d0c28e7002bc146c47
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-06 15:57:19 +0600

    update newPage

---
+++ newPage.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Testing Post Request</title>
+</head>
+<body>
+<form method="POST" action="./newPage.php">
+    Name: <input type="text" name="fname"/>
+    <input type="submit">
+</form>
+<?php
+
+?>
+</body>
+</html>
