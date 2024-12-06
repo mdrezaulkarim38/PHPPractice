ce9c4dcf5789f8f64f49e98066623f98c84112cc
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-06 15:24:20 +0600

    add cars array

--- function.php
+++ function.php
@@ -6,3 +6,60 @@ function welcome($username)
 }
 
 welcome('Shuvo');
+
+# Now about the Function Parameter
+
+function concate($str1, $str2)
+{
+    return $str1 . $str2;
+}
+
+$greeting = concate('Welcome', 'Admin');
+echo "<br > $greeting";
+
+function concat2($str1, $str2, $delimiter = ' ')
+{
+    return $str1 . $delimiter . $str2;
+}
+
+$message = concat2('Hi', 'there!');
+
+echo $message . '<br>';
+
+var_dump($message);
+
+echo '<br>';
+
+$x = array("Akbar", "Mamun", "Rony");
+var_dump($x);
+
+echo '<br>';
+
+class Student
+{
+    function department()
+    {
+        return "Physics";
+    }
+
+    function details()
+    {
+        echo $this->department();
+    }
+}
+
+$st = new Student();
+echo '<br>';
+echo $st->department();
+echo '<br>';
+$st->details();
+
+
+$cars = array("Volvo", "BMW", "Toyota");
+
+
+
+
+
+
+
